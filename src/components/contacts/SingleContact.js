cd75828606f6020ff5fe0ab527fef09ab4bcde60
Amberside <47558882+Amberside@users.noreply.github.com>
2020-09-02 10:20:42 +1000

    Update SingleContact.js
    Added redux to SingleContact.js

--- src/components/contacts/SingleContact.js
+++ src/components/contacts/SingleContact.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Consumer } from '../../context';
-import axios from 'axios';
-
+import { connect } from 'react-redux';
 
 class SingleContact extends Component {
   state = {
@@ -16,16 +14,10 @@ class SingleContact extends Component {
     this.setState({ showContactInfo: !this.state.showContactInfo });
   }
   
-  delClick = async(id, dispatch) => {
+  delClick = id => {
+    // Delete Contact
     console.log("delete: " + id);
-    //code to make sure you want to delete the contact 
-    alert('Are you sure?');
-    // this if for the old way when we passed the the function through.
-    // This calls the function in contacts.js (parent component) and will pass the id through.
-    // this.props.delContact(id);
-    // we now dispatch our action to the reducer so it can update the state.
-    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-    dispatch({ type: 'DELETE_CONTACT', payload: id });
+    // this.props.deleteContact(id);
   }
   
   // This will render our contact object passed from contacts.
@@ -35,50 +27,37 @@ class SingleContact extends Component {
     // Pull value out of the state.
     const { showContactInfo } = this.state;
     return (
-      <Consumer>
-        { value => {
-          const { dispatch } = value; // this is pulling our dispatch function in our Consumer
-          return (
-            <div className='card card-body mb-3'>
-              <h2>
-                { name }{' '}
-                <FontAwesomeIcon icon='sort-down' onClick={this.onShowClick} style={{ cursor: "pointer"}}>
-                </FontAwesomeIcon>
-                {/* This onclick call the local delClick function */}
-                <FontAwesomeIcon icon='times' style={{ cursor: "pointer", float: "right", color: "red", marginLeft: '10px'  }}
-                  onClick={this.delClick.bind(this, id, dispatch)}
-                > 
-                </FontAwesomeIcon>
-                
-                <Link to={`/contact/edit/${id}`}>
-                <FontAwesomeIcon icon='pencil-alt' 
-                    style={{ cursor: "pointer", float: "right"  }}>
-                  </FontAwesomeIcon>
-                </Link>
-              </h2>
-              { showContactInfo ? (
-                <ul className="list-group">
-                  <li className="list-group-item">Email: {email}</li>
-                  <li className='list-group-item'>Phone: {phone}</li>
-                </ul>
-              ) : null 
-              }
-            </div>
-          )}} 
-          {/* this is end the inner return and value sections */}
-      </Consumer>
-    ) // this closes the outer return
+      <div className='card card-body mb-3'>
+        <h2>
+          { name }{' '}
+          <FontAwesomeIcon icon='sort-down' onClick={this.onShowClick} style={{ cursor: "pointer"}}>
+          </FontAwesomeIcon>
+          {/* This onclick call the local delClick function */}
+          <FontAwesomeIcon icon='times' style={{ cursor: "pointer", float: "right", color: "red", marginLeft: '10px'  }}
+            onClick={this.delClick.bind(this, id)}
+          > 
+          </FontAwesomeIcon>
+          
+          <Link to={`/contact/edit/${id}`}>
+          <FontAwesomeIcon icon='pencil-alt' 
+              style={{ cursor: "pointer", float: "right"  }}>
+            </FontAwesomeIcon>
+          </Link>
+          </h2>
+          { showContactInfo ? (
+            <ul className="list-group">
+              <li className="list-group-item">Email: {email}</li>
+              <li className='list-group-item'>Phone: {phone}</li>
+            </ul>
+          ) : null 
+          }
+        </div>
+      )
   }
-}
-
-SingleContact.defaultProps = {
-  name: 'Jade Doe',
-  email: 'jadedoe@gmail.com',
-  phone: '(555)-444-5555'
 }
 
 SingleContact.propTypes = {
   contact: PropTypes.object.isRequired,
 }
 
-export default SingleContact
+export default connect(null, {  })(SingleContact)
